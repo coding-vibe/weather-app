0c8c2fbb33e15fb24820c1c4f454eccac9e2e840
merry <k.maria.m.24.05@gmail.com>
2023-08-02 15:07:05 +0300

    make main config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,8 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -19,10 +20,29 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    'jsx-quotes': ['error', 'prefer-single'],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
+    'import/prefer-default-export': [
+      ('off'),
+      { target: 'single' },
+    ],
+    'no-console': 'off',
+    'no-trailing-spaces': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx', '.ts'] }],
+    'react/no-array-index-key': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    "@typescript-eslint/no-non-null-assertion": 'off',
   },
   settings: {
     'import/resolver': {
