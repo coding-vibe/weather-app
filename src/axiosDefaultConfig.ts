8a446300dc89cc31adabcec70b99feab81e1fedb
merry <k.maria.m.24.05@gmail.com>
2023-08-03 11:47:54 +0300

    add types

--- src/axiosDefaultConfig.ts
+++ src/axiosDefaultConfig.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosDefaultConfig = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL as string,
   headers: {
-    Authorization: import.meta.env.VITE_API_KEY,
+    Authorization: import.meta.env.VITE_API_KEY as string,
   }
 });
