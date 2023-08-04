01159e7fcac76b673ceb64b40595da55302af3ab
Ihor Sokolov <i.sokolov@geniusee.com>
2023-08-04 22:21:09 +0300

    fix plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-    </>
-  );
+  return null;
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), eslint()],
   resolve: {
     alias: {
       assets: '/src/assets',
