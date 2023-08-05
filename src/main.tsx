fd695c8ebe3c32d945f42e665e0bc109ef2409b5
merry <k.maria.m.24.05@gmail.com>
2023-08-05 13:03:02 +0300

    elist fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { StyledEngineProvider } from '@mui/material/styles'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {StyledEngineProvider} from '@mui/material/styles';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <App />
     </StyledEngineProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
