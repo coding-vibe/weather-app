c92bacbd76d23e71487478c7cd350f4a2055065d
merry <k.maria.m.24.05@gmail.com>
2023-08-10 21:30:22 +0300

    fix globalstyles spreading

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,19 +1,17 @@
 export const globalStyles = {
-  styles: {
-    h1: { margin: 0 },
-    h2: { margin: 0 },
-    h3: { margin: 0 },
-    p: { margin: 0 },
-    ul: { margin: 0, padding: 0, listStyle: 'none' },
-    img: { height: 'auto', display: 'block' },
-    a: { color: 'inherit', textDecoration: 'none' },
-    button: {
-      padding: 0,
-      textAlign: 'center',
-      fontFamily: 'inherit',
-      fontSize: 'inherit',
-      lineHeight: 'inherit',
-      cursor: 'pointer',
-    },
+  h1: { margin: 0 },
+  h2: { margin: 0 },
+  h3: { margin: 0 },
+  p: { margin: 0 },
+  ul: { margin: 0, padding: 0, listStyle: 'none' },
+  img: { height: 'auto', display: 'block' },
+  a: { color: 'inherit', textDecoration: 'none' },
+  button: {
+    padding: 0,
+    textAlign: 'center',
+    fontFamily: 'inherit',
+    fontSize: 'inherit',
+    lineHeight: 'inherit',
+    cursor: 'pointer',
   },
 };
