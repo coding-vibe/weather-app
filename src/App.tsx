5165c542aaa0d9327a369d2b1c32dcd0b19660fa
merry <k.maria.m.24.05@gmail.com>
2023-08-27 13:40:53 +0300

    fix routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { SyntheticEvent, useState } from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import MUITabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import FutureForecast from 'components/FutureForecast';
@@ -9,8 +9,6 @@ import TabPanel from 'components/TabPanel/TabPanel';
 import Tabs from 'constants/tabs';
 
 export default function App() {
-  // const routes = ['/future', '/historical'];
-
   const [activeTab, setActiveTab] = useState<Tabs>(Tabs.FUTURE_FORECAST);
   const handleSelectTab = (_: SyntheticEvent, newValue: Tabs) => {
     setActiveTab(newValue);
@@ -23,29 +21,27 @@ export default function App() {
 
   return (
     <LanguageProvider>
-      <Route>
-        <MUITabs
-          value={activeTab}
-          onChange={handleSelectTab}
-          aria-label='forecast tabs'>
-          <NavLink to='future'>
-            <Tab
-              label='Future forecast'
-              {...a11yProps(Tabs.FUTURE_FORECAST)}
-            />
-          </NavLink>
-          <NavLink to='historical'>
-            <Tab
-              label='Historical forecast'
-              {...a11yProps(Tabs.HISTORICAL_FORECAST)}
-            />
-          </NavLink>
-        </MUITabs>
-      </Route>
+      <MUITabs
+        value={activeTab}
+        onChange={handleSelectTab}
+        aria-label='forecast tabs'>
+        <Tab
+          label='Future forecast'
+          to='/future forecast'
+          component={Link}
+          {...a11yProps(Tabs.FUTURE_FORECAST)}
+        />
+        <Tab
+          label='Historical forecast'
+          to='/historical forecast'
+          component={Link}
+          {...a11yProps(Tabs.HISTORICAL_FORECAST)}
+        />
+      </MUITabs>
 
       <Routes>
         <Route
-          path='future'
+          path='/future forecast'
           element={
             <TabPanel
               value={activeTab}
@@ -55,7 +51,7 @@ export default function App() {
           }
         />
         <Route
-          path='historical'
+          path='/historical forecast'
           element={
             <TabPanel
               value={activeTab}
