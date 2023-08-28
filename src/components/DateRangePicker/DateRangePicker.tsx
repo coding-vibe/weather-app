26854d0bfaec79849de792e13eb97dedc703ab5d
merry <k.maria.m.24.05@gmail.com>
2023-08-28 17:09:04 +0300

    final changes

--- src/components/DateRangePicker/DateRangePicker.tsx
+++ src/components/DateRangePicker/DateRangePicker.tsx
@@ -1,24 +1,32 @@
 import { useState } from 'react';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { FieldChangeHandlerContext } from '@mui/x-date-pickers/internals';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import MinimizeRoundedIcon from '@mui/icons-material/MinimizeRounded';
 import { startOfYesterday, subDays } from 'date-fns';
+import { useSnackbar } from 'notistack';
 import * as classes from './styles';
 
 export default function DataPicker() {
-  const [startDate, setStartDate] = useState<string | null>(null);
-  const [endDate, setEndDate] = useState<string | null>(null);
+  const { enqueueSnackbar } = useSnackbar();
   const yesterday = startOfYesterday();
   const dayBeforeYesterday = subDays(yesterday, 1);
+  const [startDate, setStartDate] = useState<Date | null>(dayBeforeYesterday);
+  const [endDate, setEndDate] = useState<Date | null>(yesterday);
 
-  const onChangeDate = (
-    value: string,
-    context: FieldChangeHandlerContext,
-  ) => {
+  const onChangeStartDate = (value: Date | null) =>
+    endDate && value && value < endDate
+      ? setStartDate(value)
+      : enqueueSnackbar('Start date must be less than end date', {
+          variant: 'error',
+        });
 
-  };
+  const onChangeEndDate = (value: Date | null) =>
+    startDate && value && startDate < value
+      ? setEndDate(value)
+      : enqueueSnackbar('End date date must be greater than start date', {
+          variant: 'error',
+        });
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -27,8 +35,10 @@ export default function DataPicker() {
         disableHighlightToday
         css={classes.datePicker}
         label='Start'
+        value={startDate}
         defaultValue={dayBeforeYesterday}
         maxDate={yesterday}
+        onChange={onChangeStartDate}
       />
       <MinimizeRoundedIcon css={classes.icon} />
       <DatePicker
@@ -37,7 +47,9 @@ export default function DataPicker() {
         css={classes.datePicker}
         label='End'
         defaultValue={yesterday}
+        value={endDate}
         maxDate={yesterday}
+        onChange={onChangeEndDate}
       />
     </LocalizationProvider>
   );
