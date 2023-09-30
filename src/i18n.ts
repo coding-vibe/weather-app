9865cfea1b6915804cc1d12b96910eb3a1081f3d
Ihor Sokolov <sihorjs@gmail.com>
2023-09-30 15:46:37 +0300

    Fix serving of translation files (#46)
    * serve translations as a public asset
    
    * add i18n http backend
    
    * update import

---
+++ src/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import Languages from 'constants/languages';
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+i18n
+  .use(initReactI18next)
+  .use(Backend)
+  .init({
+    backend: {
+      loadPath: '/locales/{{lng}}.json',
+    },
+    lng: Languages.ENGLISH,
+    fallbackLng: Languages.ENGLISH,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
