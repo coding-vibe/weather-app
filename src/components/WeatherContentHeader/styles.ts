44d2a0d86c2bbf358716ec4b3708e7ce2062e528
merry <k.maria.m.24.05@gmail.com>
2023-10-04 00:19:21 +0300

    remove to do

--- src/components/WeatherContentHeader/styles.ts
+++ src/components/WeatherContentHeader/styles.ts
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 import { Theme } from '@mui/material';
 
-// TODO: do we need it? - Yes, because we reuse this component
 export const header = (theme: Theme) => css`
   color: ${theme.palette.secondary.main};
 `;
