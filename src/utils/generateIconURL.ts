36a53d34574008613eedc1b0b2ad66c0590455d3
Ihor Sokolov <sihorjs@gmail.com>
2023-12-19 10:22:01 +0200

    Add CI/CD pipeline for deployment (#48)
    * add .env to gitignore
    
    * prepare draft workfrow
    
    * correct env variable
    
    * use env not secrets
    
    * fix mistake

--- src/utils/generateIconURL.ts
+++ src/utils/generateIconURL.ts
@@ -1,6 +1,6 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const ASSETS_BASE_URL = import.meta.env.VITE_ASSETS_BASE_URL;
 
 const generateIconURL = (iconName: string): string =>
-  `${BASE_URL}img/wn/${iconName}.png`;
+  `${ASSETS_BASE_URL}img/wn/${iconName}.png`;
 
 export default generateIconURL;
